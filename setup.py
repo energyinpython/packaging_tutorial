dad6d071ecd07f61a7a5d2151bc30e5183e897cb
energyinpython <rubinn2@wp.pl>
2022-03-16 19:31:04 +0100

    v1

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-package-energyinpython",
+    version="0.0.1",
+    author="Energy in Python",
+    author_email="rubinn2@wp.pl",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/energyinpython/packaging_tutorial",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
